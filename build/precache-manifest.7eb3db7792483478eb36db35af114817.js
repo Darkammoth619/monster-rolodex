69c2c79e7b8a924fbf7ff5ebb3264fa2c7d0c8bf
Dario <dario_cabrera1994@hotmail.com>
2020-05-20 00:07:13 -0600

    new changes

---
+++ build/precache-manifest.7eb3db7792483478eb36db35af114817.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "041f24519e707f5feaa2fc899b845c4b",
+    "url": "/monster-rolodex/index.html"
+  },
+  {
+    "revision": "0f3254daf39d2211e7cd",
+    "url": "/monster-rolodex/static/css/main.8749a770.chunk.css"
+  },
+  {
+    "revision": "2d6602caf790a4fd2697",
+    "url": "/monster-rolodex/static/js/2.1f3314c8.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/monster-rolodex/static/js/2.1f3314c8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0f3254daf39d2211e7cd",
+    "url": "/monster-rolodex/static/js/main.212980e8.chunk.js"
+  },
+  {
+    "revision": "bc09d86fba44e1c7ba83",
+    "url": "/monster-rolodex/static/js/runtime-main.8000f3b5.js"
+  }
+]);
